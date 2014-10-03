892a62888e786b4fd1001bcbd9bab51f15b0fba1
dud3 <dk18184@gmail.com>
2014-10-03 16:22:17 +0000

    create models

---
+++ app/models/emails_lists.php
@@ -0,0 +1,12 @@
+<?php
+
+class emails_list extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'emails_list';
+
+}
---
+++ app/models/keywords_list.php
@@ -0,0 +1,12 @@
+<?php
+
+class keywords_list extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'keywords_list';
+	
+}

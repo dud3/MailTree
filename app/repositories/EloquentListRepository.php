6555fa0ad295bf453dcfd77f30b5c16fce0e4889
dud3 <dk18184@gmail.com>
2014-12-10 23:35:22 +0000

    [CHANGE] Non-identical check, findWithAttr(). It seems that on keyword entity creation var type of ID is an integer, for now non-identical check. Also fix trailing spaces on the way.
    Resolves: x
    Releases: 1.2.13.8

--- app/repositories/EloquentListRepository.php
+++ app/repositories/EloquentListRepository.php
@@ -7,207 +7,207 @@ use Symfony\Component\Console\Input\InputArgument;
 
 class EloquentListRepository implements EloquentListRepositoryInterface {
 
-	protected $list_type;
+    protected $list_type;
 
-	protected $keywords;
-	protected $emails;
+    protected $keywords;
+    protected $emails;
 
-	/**
-	 * Return data.
-	 * @var stdClass
-	 */
-	protected $ret;
+    /**
+     * Return data.
+     * @var stdClass
+     */
+    protected $ret;
 
-	/**
-	 * Main Constructor.
-	 */
-	public function __construct(EloquentKeywordsRepositoryInterface $keywords, EloquentEmailsRepository $emails) {
-		$this->keywords = $keywords;
-		$this->emails = $emails;
-		$this->ret = new stdClass();
-	}
+    /**
+     * Main Constructor.
+     */
+    public function __construct(EloquentKeywordsRepositoryInterface $keywords, EloquentEmailsRepository $emails) {
+        $this->keywords = $keywords;
+        $this->emails = $emails;
+        $this->ret = new stdClass();
+    }
 
-	/**
-	 * Call the list type.
-	 * @param  [type] $type [description]
-	 * @return [type]       [description]
-	 */
-	public function __call_list($type) {
+    /**
+     * Call the list type.
+     * @param  [type] $type [description]
+     * @return [type]       [description]
+     */
+    public function __call_list($type) {
 
-		$this->list_type = $type;
+        $this->list_type = $type;
 
-		switch ($this->list_type) {
+        switch ($this->list_type) {
 
-			case 'get_email_list':
-				$this->get_email_list();
-				break;
+            case 'get_email_list':
+                $this->get_email_list();
+                break;
 
-			case 'get_keyword_list':
-				$this->get_keyword_list();
-				break;
+            case 'get_keyword_list':
+                $this->get_keyword_list();
+                break;
 
-		}
+        }
 
-	}
+    }
 
-	/**
-	 * Get list by keywords.
-	 * @return [type] [description]
-	 */
-	public function get_email_list() {
-		return $this->keywords->get_all();
-	}
+    /**
+     * Get list by keywords.
+     * @return [type] [description]
+     */
+    public function get_email_list() {
+        return $this->keywords->get_all();
+    }
 
-	/**
-	 * Get list by emails.
-	 * @return [type] [description]
-	 */
-	public function get_keyword_list() {
+    /**
+     * Get list by emails.
+     * @return [type] [description]
+     */
+    public function get_keyword_list() {
 
-	}
+    }
 
-	/**
-	 * Create Keyword list.
-	 * @return [type] [description]
-	 */
-	public function create_keywords_list($data) {
+    /**
+     * Create Keyword list.
+     * @return [type] [description]
+     */
+    public function create_keywords_list($data) {
 
-		$ret = new stdClass();
+        $ret = new stdClass();
 
-		try {
+        try {
 
-			if($data != null) {
+            if($data != null) {
 
-				if(!empty($data)) {
+                if(!empty($data)) {
 
-					/*! First of all store the keyword(s) */
-					$keywords = $this->keywords->store(["keywords" => $data["keywords"]], 
-														$original_content = (isset($data["original_content"]) ? $data["original_content"] : false) );
+                    /*! First of all store the keyword(s) */
+                    $keywords = $this->keywords->store(["keywords" => $data["keywords"]],
+                                                        $original_content = (isset($data["original_content"]) ? $data["original_content"] : false) );
 
-					if(!$keywords->error) {
+                    if(!$keywords->error) {
 
-						/* If no errors returned get the created keywords */
-						$keywords = $keywords->toArray();
+                        /* If no errors returned get the created keywords */
+                        $keywords = $keywords->toArray();
 
-						/*! To each of the recipents assign the keyword id(s) */
-						for($i = 0; $i < count($data["recipients"]); $i++) {
-							$data["recipients"][$i]["keyword_id"] = $keywords["id"];
-						}
+                        /*! To each of the recipents assign the keyword id(s) */
+                        for($i = 0; $i < count($data["recipients"]); $i++) {
+                            $data["recipients"][$i]["keyword_id"] = $keywords["id"];
+                        }
 
-					} else {
-						/** 
-						 *If error returned return the error 
-						 * the keyword variable itself will descriebe the errorto the parent class
-						 */
-						return $keywords;
-					}
+                    } else {
+                        /**
+                         *If error returned return the error
+                         * the keyword variable itself will descriebe the errorto the parent class
+                         */
+                        return $keywords;
+                    }
 
-					/* Finally store the recipients with the assigned keywords to them */
-					$recipients = $this->emails->store($data["recipients"]);
+                    /* Finally store the recipients with the assigned keywords to them */
+                    $recipients = $this->emails->store($data["recipients"]);
 
-					/* If no error returned */
-					if(!$recipients->error) {
+                    /* If no error returned */
+                    if(!$recipients->error) {
 
-						$rec_arr = [];
-						foreach ($recipients->data as $rec) {
-							$rec_arr[] = $rec->toArray();
-						}
+                        $rec_arr = [];
+                        foreach ($recipients->data as $rec) {
+                            $rec_arr[] = $rec->toArray();
+                        }
 
-						/* Return proper data, and let the js handled the rest */
-						$ret->error = $recipients->error;
-						$ret->data = ["id" => $keywords['id'], "keywords" => $keywords["keywords"], "email" => $rec_arr];
-						return $ret;
+                        /* Return proper data, and let the js handled the rest */
+                        $ret->error = $recipients->error;
+                        $ret->data = ["id" => $keywords["id"], "keywords" => $keywords["keywords"], "email" => $rec_arr];
+                        return $ret;
 
-					} else {
-						throw new RuntimeException("Error, recipients has couldn't succeed.", 0.2);
-					}
+                    } else {
+                        throw new RuntimeException("Error, recipients has couldn't succeed.", 0.2);
+                    }
 
-				} else {
-					throw new RuntimeException("Error, The array can not be empty", 0.2);
+                } else {
+                    throw new RuntimeException("Error, The array can not be empty", 0.2);
 
-				}
+                }
 
-			} else {
-				throw new RuntimeException("Errorm The array can not be null", 0.1);
-			}
+            } else {
+                throw new RuntimeException("Errorm The array can not be null", 0.1);
+            }
 
-		} catch(RuntimeException $e) {
+        } catch(RuntimeException $e) {
 
-			$error = new stdClass();
-			$error->message = $e->getMessage();
-			$error->code = $e->getCode();
-			$error->error = true;
+            $error = new stdClass();
+            $error->message = $e->getMessage();
+            $error->code = $e->getCode();
+            $error->error = true;
 
-			return $error;
+            return $error;
 
-		}
+        }
 
-	}
+    }
 
-	/**
-	 * Remove from the keywords list.
-	 * @param  [type] $id [description]
-	 * @return [type]     [description]
-	 */
-	public function remove_keywords_list($id) {
+    /**
+     * Remove from the keywords list.
+     * @param  [type] $id [description]
+     * @return [type]     [description]
+     */
+    public function remove_keywords_list($id) {
 
-		try {
+        try {
 
-			if($id != null) {
+            if($id != null) {
 
-				if(!empty($id)) {
+                if(!empty($id)) {
 
-					/*! First of all find all the users with keywords */
-					$recipients_with_keywords = $this->emails->find_recipients_by_keyword($id);
+                    /*! First of all find all the users with keywords */
+                    $recipients_with_keywords = $this->emails->find_recipients_by_keyword($id);
 
-					/* There might be keyword lists without recipients */
-					if(count($recipients_with_keywords) > 0) {
-						$recipients_with_keywords->delete();
-					} else {
-						$recipients_with_keywords = true;
-					}
+                    /* There might be keyword lists without recipients */
+                    if(count($recipients_with_keywords) > 0) {
+                        $recipients_with_keywords->delete();
+                    } else {
+                        $recipients_with_keywords = true;
+                    }
 
-					if($recipients_with_keywords) {
+                    if($recipients_with_keywords) {
 
-						/* Return proper data, and let the js handled the rest */
-						$keywords = $this->keywords->find($id);
+                        /* Return proper data, and let the js handled the rest */
+                        $keywords = $this->keywords->find($id);
 
-						/* If we're all good delete keywords as well */
-						if($keywords->delete()) {
+                        /* If we're all good delete keywords as well */
+                        if($keywords->delete()) {
 
-							$this->ret->error = false;
-							$this->ret->data = "Cool KeywordList deleted";
+                            $this->ret->error = false;
+                            $this->ret->data = "Cool KeywordList deleted";
 
-							return $this->ret;
+                            return $this->ret;
 
-						} else {
-							throw new RuntimeException("Error, recipients couldn't be deleted.", 0.2);
-						}
+                        } else {
+                            throw new RuntimeException("Error, recipients couldn't be deleted.", 0.2);
+                        }
 
-					} else {
-						throw new RuntimeException("Error, recipients couldn't be deleted.", 0.2);
-					}
+                    } else {
+                        throw new RuntimeException("Error, recipients couldn't be deleted.", 0.2);
+                    }
 
-				} else {
-					throw new RuntimeException("Error, The array can not be empty", 0.2);
+                } else {
+                    throw new RuntimeException("Error, The array can not be empty", 0.2);
 
-				}
+                }
 
-			} else {
-				throw new RuntimeException("Errorm The array can not be null", 0.1);
-			}
+            } else {
+                throw new RuntimeException("Errorm The array can not be null", 0.1);
+            }
 
-		} catch(RuntimeException $e) {
+        } catch(RuntimeException $e) {
 
-			$error = new stdClass();
-			$error->message = $e->getMessage();
-			$error->code = $e->getCode();
-			$error->error = true;
+            $error = new stdClass();
+            $error->message = $e->getMessage();
+            $error->code = $e->getCode();
+            $error->error = true;
 
-			return $error;
+            return $error;
 
-		}
+        }
 
-	}
+    }
 
 }
--- public/scripts/controllers/keyWordsListCtrl.js
+++ public/scripts/controllers/keyWordsListCtrl.js
@@ -485,7 +485,7 @@ angular.module('app.keyWordsList')
 		 */
 		$scope.findWithAttr = function(array, attr, value) {
 			for(var i = 0; i < array.length; i += 1) {
-				if(array[i][attr] === value) {
+				if(array[i][attr] == value) {
 				    return i;
 				}
 			}
